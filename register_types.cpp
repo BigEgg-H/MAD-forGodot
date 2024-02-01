0c6b00401df1c537224b3b0456aa13a9e1edbd32
BigEgg-H <1515015820@qq.com>
2024-02-01 17:04:11 +0800

    Initial commit

---
+++ register_types.cpp
@@ -0,0 +1,11 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+
+#include "core/object/class_db.h"
+
+void initialize_mad_module(ModuleInitializationLevel p_level) {
+}
+
+void uninitialize_mad_module(ModuleInitializationLevel p_level) {
+}
---
+++ register_types.h
@@ -0,0 +1,6 @@
+/* register_types.h */
+
+#include "modules/register_module_types.h"
+
+void initialize_mad_module(ModuleInitializationLevel p_level);
+void uninitialize_mad_module(ModuleInitializationLevel p_level);
